c1468c84b474c541e7dd0cc04376c1ff8653e5de
punit1808 <punityadav1808@gmail.com>
2026-05-30 03:13:11 +0530

    Docker build push workflow and ratelimiting

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
+import rateLimit from "express-rate-limit";
 
 import { createProxyMiddleware } from "http-proxy-middleware";
 
@@ -11,7 +12,36 @@ import authMiddleware from "./middleware/authMiddleware.js";
 
 const app = express();
 
-app.use(cors());
+app.use(
+    cors({
+        origin: [
+            process.env.FRONTEND_URL_LIST
+        ],
+        credentials: true,
+        methods: [
+            "GET",
+            "POST",
+            "PUT",
+            "DELETE",
+            "OPTIONS"
+        ],
+        allowedHeaders: [
+            "Authorization",
+            "Content-Type"
+        ]
+    })
+);
+const apiLimiter = rateLimit({
+    windowMs: Number(process.env.RATE_LIMIT_WINDOW),
+    max: Number(process.env.RATE_LIMIT),
+    standardHeaders: true,
+    legacyHeaders: false,
+    message: {
+        message: "Too many requests. Please try again later."
+    }
+});
+
+app.use(apiLimiter);
 app.use(helmet());
 app.use(morgan("dev"));
 
@@ -47,6 +77,27 @@ app.use(
 
 app.use(
     "/api",
+    createProxyMiddleware({
+        target: process.env.BACKEND_SERVICE_URL,
+        changeOrigin: true
+    })
+);
+app.use(
+    "/location",
+    createProxyMiddleware({
+        target: process.env.BACKEND_SERVICE_URL,
+        changeOrigin: true
+    })
+);
+app.use(
+    "/group",
+    createProxyMiddleware({
+        target: process.env.BACKEND_SERVICE_URL,
+        changeOrigin: true
+    })
+);
+app.use(
+    "/user",
     createProxyMiddleware({
         target: process.env.BACKEND_SERVICE_URL,
         changeOrigin: true
